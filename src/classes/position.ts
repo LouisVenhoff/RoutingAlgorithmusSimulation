2db3f082803251e7ecfb9e7d7a39cb0df88792df
DESKTOP-OH4MRFL\Louis Venhoff <Louis.venhoff@gmx.net>
2023-02-25 02:48:41 +0100

    InitialCommit

---
+++ src/classes/position.ts
@@ -0,0 +1,118 @@
+
+type PositionCords = 
+{
+    x:number;
+    y:number;
+}
+
+type PositionMarker = 
+{
+    pos:Position;
+    distance:number;
+}
+
+class Position  
+{
+    private maxX:number;
+    private maxY:number;
+    private connectCount:number;
+
+    private xPosition:number = 0;
+    private yPosition:number = 0;
+
+    private neighbours:Position[] = [];
+
+
+    constructor(maxX:number, maxY:number, connectCount:number)
+    {
+       this.maxX = maxX;
+       this.maxY = maxY;
+       this.connectCount = connectCount;
+       this.generatePosition();
+       
+    }
+
+    public getPosition():PositionCords
+    {
+        return{x:this.xPosition, y:this.yPosition}
+    }
+
+    public getNeighbours(allPos:Position[]):Position[]
+    {
+        if(this.neighbours.length > 0)
+        {
+            return this.neighbours;
+        }
+        
+        this.processNeighbours(allPos);
+        
+        return this.neighbours;
+
+    }
+
+    private processNeighbours(allPos:Position[])
+    {
+        
+        let calculatedPositions:PositionMarker[] = [];
+        
+        for(let i = 0; i < allPos.length; i++)
+        {
+            calculatedPositions.push(this.createPositionMarker(allPos[i]));
+        }
+
+        calculatedPositions.sort((a:PositionMarker, b:PositionMarker) => {return a.distance - b.distance});
+
+        for(let j = 0; j < this.connectCount; j++)
+        {
+            this.neighbours.push(calculatedPositions[j].pos);
+        }
+    }
+
+    private createPositionMarker(currentPos:Position):PositionMarker
+    {
+        let targetCords:PositionCords = currentPos.getPosition();
+        
+        let dist:number = this.calculateDistanceTo(targetCords.x, targetCords.y);
+
+        let outObj = {pos:currentPos, distance:dist}
+
+        return outObj;
+
+    }
+
+    private calculateDistanceTo(x:number, y:number):number
+    {
+        let xDiff:number = this.diffCords(this.xPosition, x);  
+        let yDiff:number = this.diffCords(this.yPosition, y);
+        
+        let tempResult:number = Math.pow(xDiff, 2) + Math.pow(yDiff,2);
+
+        return Math.sqrt(tempResult);
+        
+    }
+
+    private diffCords(cordOne:number, cordTwo:number):number
+    {
+        let temp:number = cordOne - cordTwo;
+
+        if(temp < 0)
+        {
+            return Math.abs(temp);
+        }
+        else
+        {
+            return temp;
+        }
+    }
+
+
+    private generatePosition()
+    {
+        this.xPosition = (Math.random() * (this.maxX));
+        this.yPosition = (Math.random() * (this.maxY));
+    }
+
+}
+
+export type {PositionCords};
+export default Position;
---
+++ src/components/world/terrain.tsx
@@ -0,0 +1,108 @@
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
+import Position, { PositionCords } from "../../classes/position";
+import "./terrainStyle.css";
+
+type TerrainProps = 
+{
+    posCount:number;
+}
+
+
+
+const Terrain:React.FC<TerrainProps> = ({posCount}) => 
+{
+    
+    const [posList, setPosList] = useState<Position[]>([]);
+    
+
+    useEffect(() => {
+        clearCanvas();
+        generatePositions(posCount);
+        drawCanvasPositions();
+        drawRelations();
+    },[]);
+
+    useEffect(() => {
+
+    },[posCount]);
+
+    
+
+    const generatePositions = (count:number) => 
+    {
+        setPosList([]);  
+        for(let i = 0; i < count; i++)
+        {
+            posList.push(new Position(1000, 600, 3));
+        }
+    }
+
+    const drawRelations = () => 
+    {
+       
+        for(let i = 0; i < posList.length; i++)
+        {
+            let startPoint:PositionCords = posList[i].getPosition();
+
+            let neighbours:Position[] = posList[i].getNeighbours(posList);
+            
+            for(let j = 0; j < neighbours.length; j++)
+            {
+                drawLine(startPoint, neighbours[j].getPosition());
+            }
+        }
+    }
+
+    const getCanvas = ():HTMLElement | null => 
+    {
+        return document.getElementById("screen");
+    }
+
+    const clearCanvas = () => 
+    {
+        let canvas:HTMLCanvasElement = getCanvas() as HTMLCanvasElement;
+        let context:any = canvas.getContext("2d");
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    const drawItem = (posObj:PositionCords) =>
+    {
+        let canvas:any = getCanvas();
+        let drawingContext:CanvasRenderingContext2D = canvas.getContext("2d");
+        drawingContext.fillStyle = "white";
+        drawingContext.fillRect(posObj.x, posObj.y, 10, 10); 
+        
+    }
+
+    const drawLine = (start:PositionCords, target:PositionCords) => 
+    {
+        console.log("Drawing Line");
+        let canvas:any = getCanvas();
+        let drawingContext:CanvasRenderingContext2D = canvas.getContext("2d");
+        drawingContext.strokeStyle = "red";
+        drawingContext.moveTo(start.x, start.y);
+        drawingContext.lineTo(target.x, target.y );
+        drawingContext.stroke();
+        drawingContext.closePath();
+    }
+
+    const drawCanvasPositions = () => 
+    {
+        for(let i = 0; i < posList.length; i++)
+        {
+            drawItem(posList[i].getPosition())
+        }
+    }
+
+    return(
+    <div className="terrainDiv">
+         <div className="world">
+            <canvas id="screen"  width="1000" height="600"/>
+
+        </div>
+    </div>
+   
+    );
+}
+
+export default Terrain;
